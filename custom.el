e99a65df6268c028a5c890d7abfc6f3d15d45d96
Werner Kok <wrk@juxt.pro>
2021-07-19 16:07:12 +0100

    Remove custom bindings

--- custom.el
+++ custom.el
@@ -1,25 +1,25 @@
-;; Set font size
+1;; Set font size
 (set-face-attribute 'default nil :height 200)
 
-(add-hook 'clojure-mode-hook
-  (lambda ()
-    (local-set-key (kbd "M-(") 'paredit-wrap-round)))
+;; (add-hook 'clojure-mode-hook
+;;   (lambda ()
+;;     (local-set-key (kbd "M-(") 'paredit-wrap-round)))
 
-(add-hook 'clojure-mode-hook
-  (lambda ()
-    (local-set-key (kbd "M-[") 'paredit-wrap-square)))
+;; (add-hook 'clojure-mode-hook
+;;   (lambda ()
+;;     (local-set-key (kbd "M-[") 'paredit-wrap-square)))
 
-(add-hook 'clojure-mode-hook
-  (lambda ()
-    (local-set-key (kbd "M-{") 'paredit-wrap-curly)))
+;; (add-hook 'clojure-mode-hook
+;;   (lambda ()
+;;     (local-set-key (kbd "M-{") 'paredit-wrap-curly)))
 
-(add-hook 'clojure-mode-hook
-  (lambda ()
-    (local-set-key (kbd "C-w") 'sp-kill-sexp)))
+;; (add-hook 'clojure-mode-hook
+;;   (lambda ()
+;;     (local-set-key (kbd "C-w") 'sp-kill-sexp)))
 
-(add-hook 'clojure-mode-hook
-  (lambda ()
-    (local-set-key (kbd "M-w") 'sp-copy-sexp)))
+;; (add-hook 'clojure-mode-hook
+;;   (lambda ()
+;;     (local-set-key (kbd "M-w") 'sp-copy-sexp)))
 
 ;; (define-key smartparens-mode-map (kbd "M-(") 'sp-wrap-round)
 ;; (define-key smartparens-mode-map (kbd "M-[") 'sp-wrap-square)
@@ -31,18 +31,18 @@
 
 ;; Indentation of function forms
 ;; https://github.com/clojure-emacs/clojure-mode#indentation-of-function-forms
-(setq clojure-indent-style 'align-arguments)
+;; (setq clojure-indent-style 'align-arguments)
 
 ;; Vertically align s-expressions
 ;; https://github.com/clojure-emacs/clojure-mode#vertical-alignment
-(setq clojure-align-forms-automatically t)
+;; (setq clojure-align-forms-automatically t)
 
 ;; Auto-indent code automatically
 ;; https://emacsredux.com/blog/2016/02/07/auto-indent-your-code-with-aggressive-indent-mode/
 ;; (add-hook 'clojure-mode-hook #'aggressive-indent-mode)
 
 ;; (setq ns-pop-up-frames nil) ;opens files in same frame
-(desktop-save-mode)
-(auto-save-visited-mode)
+;; (desktop-save-mode)
+;; (auto-save-visited-mode)
 ;; (golden-ratio-mode)
 ;; (desktop-read)
