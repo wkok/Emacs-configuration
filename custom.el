0af9e52d221fab6f9d476f002553270281377685
Werner Kok <wrk@juxt.pro>
2021-07-26 08:43:13 +0100

    Enable setting from my Spacemacs config

--- custom.el
+++ custom.el
@@ -8,6 +8,9 @@
 (setq plantuml-jar-path "~/bin/plantuml/plantuml.jar")
 (setq plantuml-default-exec-mode 'jar)
 
+;; Enable plantuml-mode for PlantUML files
+(add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
+
 ;; (add-hook 'clojure-mode-hook
 ;;   (lambda ()
 ;;     (local-set-key (kbd "M-(") 'paredit-wrap-round)))
@@ -33,23 +36,26 @@
 ;; (define-key smartparens-mode-map (kbd "M-{") 'sp-wrap-curly)
 ;; (define-key smartparens-mode-map (kbd "C-<right>") 'sp-forward-slurp-sexp)
 ;; (define-key smartparens-mode-map (kbd "C-<left>") 'sp-forward-barf-sexp)
-;; (define-key smartparens-mode-map (kbd "C-w") 'sp-kill-sexp)
-;; (define-key smartparens-mode-map (kbd "M-w") 'sp-copy-sexp)
+(define-key smartparens-mode-map (kbd "C-w") 'sp-kill-sexp)
+(define-key smartparens-mode-map (kbd "M-w") 'sp-copy-sexp)
 
 ;; Indentation of function forms
 ;; https://github.com/clojure-emacs/clojure-mode#indentation-of-function-forms
-;; (setq clojure-indent-style 'align-arguments)
+ (setq clojure-indent-style 'align-arguments)
 
 ;; Vertically align s-expressions
 ;; https://github.com/clojure-emacs/clojure-mode#vertical-alignment
-;; (setq clojure-align-forms-automatically t)
+ (setq clojure-align-forms-automatically t)
 
 ;; Auto-indent code automatically
 ;; https://emacsredux.com/blog/2016/02/07/auto-indent-your-code-with-aggressive-indent-mode/
-;; (add-hook 'clojure-mode-hook #'aggressive-indent-mode)
+(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
 
-;; (setq ns-pop-up-frames nil) ;opens files in same frame
-;; (desktop-save-mode)
-;; (auto-save-visited-mode)
-;; (golden-ratio-mode)
-;; (desktop-read)
+(setq ns-pop-up-frames nil) ;opens files in same frame
+(desktop-save-mode)
+(auto-save-visited-mode)
+(golden-ratio-mode)
+(desktop-read)
+
+(provide 'custom)
+;;; custom.el ends here
