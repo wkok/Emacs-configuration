6edc7dd110830bd1b43a9dd7d7e5f206dff93084
Werner Kok <wernerkok@gmail.com>
2021-08-19 13:45:06 +0100

    clean up custom config

--- custom.el
+++ custom.el
@@ -1,61 +1,38 @@
-1;; Set font size
-(set-face-attribute 'default nil :height 200)
+;;; custom --- Custom configuration
 
-(smartparens-global-strict-mode t)
+;;; Commentary:
 
+;;; Code:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Look & Feel
+;;;
+;;; (set-face-attribute 'default nil :height 100)
 (load-theme 'dracula t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Clojure
+;;;
+;;; (smartparens-global-strict-mode t)
+;;;
+;;; on windows clojure-lsp is on path so this can be nil
+(setq lsp-clojure-server-command nil)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Plant UML
+;;;
 (setq plantuml-jar-path "~/bin/plantuml/plantuml.jar")
 (setq plantuml-default-exec-mode 'jar)
-
-;; Enable plantuml-mode for PlantUML files
+;;; Enable plantuml-mode for PlantUML files
 (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; (add-hook 'clojure-mode-hook
-;;   (lambda ()
-;;     (local-set-key (kbd "M-(") 'paredit-wrap-round)))
 
-;; (add-hook 'clojure-mode-hook
-;;   (lambda ()
-;;     (local-set-key (kbd "M-[") 'paredit-wrap-square)))
-
-;; (add-hook 'clojure-mode-hook
-;;   (lambda ()
-;;     (local-set-key (kbd "M-{") 'paredit-wrap-curly)))
-
-;; (add-hook 'clojure-mode-hook
-;;   (lambda ()
-;;     (local-set-key (kbd "C-w") 'sp-kill-sexp)))
-
-;; (add-hook 'clojure-mode-hook
-;;   (lambda ()
-;;     (local-set-key (kbd "M-w") 'sp-copy-sexp)))
-
-;; (define-key smartparens-mode-map (kbd "M-(") 'sp-wrap-round)
-;; (define-key smartparens-mode-map (kbd "M-[") 'sp-wrap-square)
-;; (define-key smartparens-mode-map (kbd "M-{") 'sp-wrap-curly)
-;; (define-key smartparens-mode-map (kbd "C-<right>") 'sp-forward-slurp-sexp)
-;; (define-key smartparens-mode-map (kbd "C-<left>") 'sp-forward-barf-sexp)
-(define-key smartparens-mode-map (kbd "C-w") 'sp-kill-sexp)
-(define-key smartparens-mode-map (kbd "M-w") 'sp-copy-sexp)
-
-;; Indentation of function forms
-;; https://github.com/clojure-emacs/clojure-mode#indentation-of-function-forms
- (setq clojure-indent-style 'align-arguments)
-
-;; Vertically align s-expressions
-;; https://github.com/clojure-emacs/clojure-mode#vertical-alignment
- (setq clojure-align-forms-automatically t)
-
-;; Auto-indent code automatically
-;; https://emacsredux.com/blog/2016/02/07/auto-indent-your-code-with-aggressive-indent-mode/
-(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
-
-(setq ns-pop-up-frames nil) ;opens files in same frame
-(desktop-save-mode)
 (auto-save-visited-mode)
-(golden-ratio-mode)
-(desktop-read)
 
 (provide 'custom)
 ;;; custom.el ends here
